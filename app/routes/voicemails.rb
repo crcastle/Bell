86fdb03c41fe3130ad9e289c26847c2a61bfcc98
Chris Castle <crcastle@gmail.com>
2010-11-14 18:03:13 -0800

    started voicemail route structure

--- app/routes/voicemails.rb
+++ app/routes/voicemails.rb
@@ -1,5 +1,32 @@
 class Main
+  # list voicemails
   get "/voicemails" do
+    # list all voicemails in DB for current_user
+  end
+  
+  # create voicemail
+  post "/voicemails" do
+    # create a voicemail in DB for user determined by number called
+    # mark new voicemail as "new"
+  end  
 
+  # play voicemail
+  get "/voicemails/:id" do
+    # check if voicemail :id belongs to current_user
+    # show voicemail details with option to play and delete
+  end
+  
+  # play voicemail
+  get "/voicemails/:id/play"
+    # check if voicemail :id belongs to current_user
+    # play voicemail :id
+    # mark voicemail :id as "not new"
+  end
+  
+  # delete voicemail
+  post "/voicemails/:id" do
+    # check if voicemail :id belongs to current_user
+    # delete voicemail from voicemail storage location
+    # delete voicemail from DB
   end
 end
