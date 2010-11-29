e323f635bd227b5e3e1445c83c980942e7741fc9
Chris Castle <crcastle@gmail.com>
2010-11-29 13:37:45 -0800

    removed hard-coded URLs referring to app's current url

--- app/routes/cloudvox_call_control.rb
+++ app/routes/cloudvox_call_control.rb
@@ -18,7 +18,7 @@ class Main
     @phrase = "Please press the 1 key to hear your phone's verification code."
     @max_digits = "1"
     @timeout = "2"
-    @callback_url = "http://bell.heroku.com/cv/verify_land"
+    @callback_url = request.url
     @json_output = [{:name => "Speak", :phrase => @phrase},{:name => "GetDigits", :max => @max_digits, :timeout => @timeout, :url => @callback_url}].to_json
   end
   
